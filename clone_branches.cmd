68dd6ac647470a35f1733365e75b1e127afd76b5
RUTVIK-2002 <89594002+RUTVIK-2002@users.noreply.github.com>
2023-07-16 23:22:39 +0530

    Add files via upload
    In the above cmd file is the code to download data from all git branches.
    
    Steps to follow:
    1. Download the file and open it in notepad or any editor 
    2. Change <repository name> to the name of the github file
    3. Save it and exit
    4. Run the cmd file

---
+++ clone_branches.cmd
@@ -0,0 +1,30 @@
+@echo off
+
+REM Prompt user for GitHub repository link
+set /p github_link=Enter GitHub repository link: 
+
+REM Clone the repository
+git clone %github_link%
+REM Make User to Enter name of the Folder
+cd <respository_name>
+
+REM Fetch all branches
+git fetch --all
+
+REM Get the list of all branches
+for /f "delims=" %%b in ('git branch -r ^| findstr /v "HEAD"') do (
+  REM Trim branch name
+  set "branch=%%b"
+  setlocal enabledelayedexpansion
+  set "branch=!branch:~9!"
+  
+  REM Checkout the branch
+  echo Fetching branch: !branch!
+  git checkout !branch!
+  
+  REM Do whatever you want with the branch here
+  REM Example: Copy the files to a different directory
+  xcopy /s /e /y .\source_directory .\destination_directory\!branch!
+  
+  endlocal
+)
